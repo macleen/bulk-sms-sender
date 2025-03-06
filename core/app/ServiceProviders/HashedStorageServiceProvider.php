ca55263bfee1c5a1f3426b1a1005a6353178ba1b
macleen <zuidman@ymail.com>
2025-03-06 13:02:33 +0100

    Added some comments

--- core/app/ServiceProviders/HashedStorageServiceProvider.php
+++ core/app/ServiceProviders/HashedStorageServiceProvider.php
@@ -5,6 +5,5 @@ use App\Support\HashedStorage;
 class HashedStorageServiceProvider extends ServiceProvider {
 
     public function boot(): void {
-        $this->plugin_kernel->bind(HashedStorage::class, fn() => new HashedStorage( ) );
     }
 }
